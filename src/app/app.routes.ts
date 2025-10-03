78b9739a155bda818dd19d33a404f4020162456e
Schuster Phillip <schusterphillip63@gmail.com>
2025-10-03 22:43:21 +0200

    feat:change of the styling of the intro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './layouts/auth-layout-component/auth-layout-component';
 import { WorkspaceLayoutComponent } from './layouts/workspace-layout-component/workspace-layout-component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 export const routes: Routes = [
   { path: '', component: AuthLayoutComponent },
   { path: 'workspace', component: WorkspaceLayoutComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'imprint', component: ImpressumComponent },
 ];
