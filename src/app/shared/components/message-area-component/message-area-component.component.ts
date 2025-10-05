2119424efeb9abb0cb202bdfd5af29b99495e99b
Schuster Phillip <schusterphillip63@gmail.com>
2025-10-05 15:37:11 +0200

    feat: implement of the reusable message-area

---
+++ src/app/shared/components/message-area-component/message-area-component.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-message-area-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './message-area-component.component.html',
+  styleUrl: './message-area-component.component.scss'
+})
+export class MessageAreaComponentComponent {
+
+}
