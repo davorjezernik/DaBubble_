aa8074f7f327475f944033111ebe39e52e7a01d9
CeciliaDiesch <ceciliadiesch@yahoo.com>
2025-10-09 19:26:21 +0200

    Message-Bubble Layout

--- src/app/features/workspace/components/chat-interface-content/chat-interface-content.ts
+++ src/app/features/workspace/components/chat-interface-content/chat-interface-content.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MessageAreaComponent } from '../../../../shared/components/message-area-component/message-area-component';
+import { MessageBubbleComponent } from '../../../../shared/components/message-bubble-component/message-bubble.component';
 
 @Component({
   selector: 'app-chat-interface-component',
-  imports: [MessageAreaComponent],
+  imports: [MessageAreaComponent, MessageBubbleComponent],
   templateUrl: './chat-interface-content.html',
   styleUrl: './chat-interface-component.scss',
 })
---
+++ src/app/shared/components/message-bubble-component/message-bubble.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-message-bubble',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './message-bubble.component.html',
+  styleUrl: './message-bubble.component.scss'
+})
+export class MessageBubbleComponent {
+  @Input() name: string = 'Frederik Beck';
+  @Input() time: string = '15:06 Uhr';
+  @Input() avatar: string = 'assets/img-profile/frederik-beck.png';
+  @Input() text: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque blandit odio efficitur lectus vestibulum, quis accumsan ante vulputate. Quisque tristique iaculis erat, eu faucibus lacus iaculis ac.';
+}
