c4d9db9be8650b47b362c0695142244bf0665a74
Mark Corzilius <marccorzprivate@gmail.com>
2025-10-21 16:58:03 +0200

    feat(mention-list): display users based on input in create-channel dialog (click-to-select pending)

--- src/app/features/workspace/components/add-users-to-channel/add-users-to-channel.ts
+++ src/app/features/workspace/components/add-users-to-channel/add-users-to-channel.ts
@@ -1,5 +1,5 @@
 import { DialogRef } from '@angular/cdk/dialog';
-import { Component } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -7,21 +7,51 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
-import { MentionListComponent } from "../../../../shared/components/mention-list.component/mention-list.component";
+import {
+  MentionListComponent,
+  MentionUser,
+} from '../../../../shared/components/mention-list.component/mention-list.component';
+import { UserService } from '../../../../../services/user.service';
+import { user } from '@angular/fire/auth';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-add-users-to-channel',
-  imports: [MatDialogModule, MatIconModule, MatRadioModule, MatButtonModule, FormsModule, MatFormFieldModule, MatInputModule, MentionListComponent],
+  imports: [
+    MatDialogModule,
+    MatIconModule,
+    MatRadioModule,
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MentionListComponent,
+  ],
   templateUrl: './add-users-to-channel.html',
   styleUrls: ['./add-users-to-channel.scss', '../../../../shared/styles/form-field-styles.scss'],
 })
-export class AddUsersToChannel {
-
+export class AddUsersToChannel implements OnInit {
   selectedOption: string = 'all';
   users: any[] = [];
-  inputValue: string = "";
+  inputValue: string = '';
 
-  constructor(public dialogRef: DialogRef<AddUsersToChannel>) {
+  get showMention(): boolean {
+    return this.inputValue.length > 0;
+  }
+
+  mentionMode: 'users' | 'channels' = 'users';
+  mentionUsers: MentionUser[] = [];
+
+  constructor(public dialogRef: DialogRef<AddUsersToChannel>, private userService: UserService) {}
+
+  async ngOnInit() {
+    const users = await firstValueFrom(this.userService.users$());
+    this.mentionUsers = users.map((u) => ({
+      uid: u.uid,
+      name: u.name,
+      avatar: u.avatar,
+      online: u.online,
+    }));
   }
 
   onConfirm() {
