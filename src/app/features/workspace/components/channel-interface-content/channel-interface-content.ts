d40d9d009664e20cb846c57aef76170ca826a49e
Mark Corzilius <marccorzprivate@gmail.com>
2025-10-23 22:10:56 +0200

    style(channel-interface): adjust header styling in content component

--- src/app/features/workspace/components/channel-interface-content/channel-interface-content.ts
+++ src/app/features/workspace/components/channel-interface-content/channel-interface-content.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MessageAreaComponent } from "../../../../shared/components/message-area-component/message-area-component";
 
 @Component({
   selector: 'app-channel-interface-content',
-  imports: [],
+  imports: [MessageAreaComponent],
   templateUrl: './channel-interface-content.html',
   styleUrl: './channel-interface-content.scss'
 })
 export class ChannelInterfaceContent {
-
 }
