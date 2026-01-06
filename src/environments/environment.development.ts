b0cdbef7e7adefcd6e4522260f1ac0ecf325c1d9
davorjezernik <369davor@gmail.com>
2026-01-06 15:35:41 +0100

    changes for environment for using another firebase project

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBT81psRiMumvUIdzN1EAwK8NjKIEYUPM0',
-    authDomain: 'dabubble-69d59.firebaseapp.com',
-    databaseURL: 'https://dabubble-69d59-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'dabubble-69d59',
-    storageBucket: 'dabubble-69d59.firebasestorage.app',
-    messagingSenderId: '69214031220',
-    appId: '1:69214031220:web:3d2480569252b62ac03021',
+    apiKey: 'AIzaSyBMmQx2pReGrZbyCuI21HPfsX5kI2SRcOg',
+    authDomain: 'dabubble-my.firebaseapp.com',
+    databaseURL: 'https://dabubble-my-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'dabubble-my',
+    storageBucket: 'dabubble-my.firebasestorage.app',
+    messagingSenderId: '769661192588',
+    appId: '1:769661192588:web:852571800c9baa00bdeea9',
   },
   guest: {
     email: 'guest@example.com',
